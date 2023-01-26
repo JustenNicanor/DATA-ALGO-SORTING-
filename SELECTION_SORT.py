1219edb11f2b99a6a4774fc176d3ab8687dc8f91
JustenNicanor <92636904+JustenNicanor@users.noreply.github.com>
2023-01-27 00:33:00 +0800

    Add files via upload

---
+++ SELECTION_SORT.py
@@ -0,0 +1,15 @@
+arr = [97, 60, 45, 96, 33, 19, 53, 37, 29, 98]
+
+def selection_sort(arr):
+    for i in range(0, len(arr) - 1):
+        cur_min_idx = i
+        for j in range(i + 1, len(arr)):
+            if arr[j] < arr[cur_min_idx]:
+                cur_min_idx = j
+
+        arr[i], arr[cur_min_idx] = arr[cur_min_idx], arr[i]
+
+
+
+selection_sort(arr)
+print(arr)
